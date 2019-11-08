00da5fd55845413a847c840d912690c698dca253
zachary <zblumstein@gmail.com>
2019-11-08 05:25:02 -0500

    added new deppendency:     "react-router-dom": "4.1.1",

--- src/App.js
+++ src/App.js
@@ -7,21 +7,43 @@ import "./App.css";
 import HeaderNavigation from "../src/HeaderNavigation";
 import Form from "../src/Form";
 import Body from "../src/Body";
+import Home from "../src/Home";
+import About from "../src/About";
+import More from "../src/More";
+import {BrowserRouter, Route } from "react-router-dom";
 
-const App = () => (
+const App = () => <>
     <>
-        <>
-            <HeaderNavigation className="body"/>
-            <Main/>
-        </>
-        <Jumbotron fluid>
-            <h1 className="header">Welcome To React-Bootstrap</h1>
-        </Jumbotron>
-        <Jumbotron fluid>
-            <Form className="header"/>
-        </Jumbotron>
-        <Body className="body"/>
+        <HeaderNavigation className="body"/>
+        <Main/>
+
     </>
-);
+    <Jumbotron fluid>
+        <h1 className="header">Welcome To React-Bootstrap</h1>
+    </Jumbotron>
+    <Jumbotron fluid>
+        <Form className="header"/>
+
+    </Jumbotron>
+    <Body className="body">
+        <BrowserRouter>
+            <div>
+                <Route exact={true} path='/'
+                       render={() => <div className="App">
+                    <Home/>
+                </div>}/>
+                <Route exact={true} path='/More'
+                       render={() => <div className="App">
+                    <More/>
+                </div>}/>
+                <Route exact={true} path='/About'
+                       render={() => <div className="App">
+                    <About/>
+                </div>}/>
+            </div>
+        </BrowserRouter>
+
+    </Body>
+</>;
 
 export default App;
