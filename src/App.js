c93fc2e84e72d768d141a0076c519e74f41e47ad
Zachary Blumstein <32861845+Blummer92@users.noreply.github.com>
2019-11-08 02:38:49 -0500

    added routers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import Main from "./Main";
 
 // Importing the Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,20 +8,21 @@ import "./App.css";
 import HeaderNavigation from "../src/HeaderNavigation";
 import Form from "../src/Form";
 import Body from "../src/Body";
-import AddTripButton from "../src/Triggerstate";
 
 const App = () => (
-  <div>
-    <HeaderNavigation className="body" />
+  <>
+    <>
+      <HeaderNavigation className="body" />
+      <Main />
+    </>
     <Jumbotron fluid>
       <h1 className="header">Welcome To React-Bootstrap</h1>
     </Jumbotron>
     <Jumbotron fluid>
-      <AddTripButton addTrip={this.triggerAddTripState} />
       <Form className="header" />
     </Jumbotron>
     <Body className="body" />
-  </div>
+  </>
 );
 
 export default App;
